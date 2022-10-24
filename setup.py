f39be5fd7cb0fb4b754774cbeaab398a82ab41e0
Mirza Kapetanovic <mirza.kapetanovic@gmail.com>
2022-10-24 12:51:25 +0200

    v1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='rotary',
-    version='1.0.0',
+    version='1.1.0',
     description='A Python driver to read a rotary encoder.',
     long_description=long_description,
     long_description_content_type='text/markdown',
